066ef4b636e0405ad6c0e6082d5205c3cdc1e51f
Guillermo Guevara <guillermoguevara887@gmail.com>
2022-12-08 22:52:14 -0800

    agregue mas funciones

--- cuadrado.js
+++ cuadrado.js
@@ -3,3 +3,20 @@ function cuadrado(lado1,lado2){
 }
 const resultado = cuadrado(3,6);
 console.log('El resultado es: ' + resultado);
+
+function calcularCuadrado(lado) {
+    return {
+      perimetro: lado * 4,
+      area: lado * lado,
+    };
+  }
+  console.log(calcularCuadrado(5));
+
+  function horas(dineroHoras, Horas ){
+      return {
+        horas: Horas,
+        pago: (dineroHoras * Horas),
+      };
+  }
+
+  console.log(horas(21,65));
